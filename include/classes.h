0563018c53e956b31228ae918b91465d36332775
isabelamendesx <gomesisabela13@gmail.com>
2023-06-13 18:57:17 -0300

    Adição das primeiras classes

--- include/classes.h
+++ include/classes.h
@@ -1,18 +1,60 @@
 #ifndef CLASSES_H
 #define CLASSES_H
+#define MAX 100
 #include <iostream>
 #include "functions.h"
 //#include "estruturas_tads.hpp"
 
-class Tarefas{
-    public:
+class Tarefa{
+    protected:
         std::string titulo;
         std::string descricao;
         std::string status;
         int prioridade;
         int dia, mes, ano; //data de vencimento
-    private:
+    public:
+        Tarefa();
+        Tarefa(const std::string& titulo, const std::string descricao, const std::string status, int prioridade, int dia, int mes, int ano);
+
 };
+
+class Coluna {
+protected: 
+
+public:
+    void adicionarTarefa();
+    void removerTarefa();
+    void moverTarefa(const Tarefa& tarefa, Coluna& colunaDestino);
+    
+};
+
+class Backlog : public Coluna {
+private:
+    Tarefa* listabacklog;
+public:
+
+};
+
+class EmAnalise : public Coluna {
+public:
+
+};
+
+class ParaFazer : public Coluna {
+public:
+
+};
+
+class Fazendo : public Coluna {
+public:
+
+};
+
+class Feito : public Coluna {
+public:
+
+};
+
 
 
 
