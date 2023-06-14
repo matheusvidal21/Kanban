1f7944b15e4c63a7cb0d7879626d39d4b7d2e7b6
isabelamendesx <gomesisabela13@gmail.com>
2023-06-14 15:29:43 -0300

    Classes adicionadas

--- include/classes.h
+++ include/classes.h
@@ -1,64 +1,84 @@
 #ifndef CLASSES_H
 #define CLASSES_H
-#define MAX 100
 #include <iostream>
 #include "functions.h"
-//#include "estruturas_tads.hpp"
+#include "estruturas_tads.hpp"
 
 class Tarefa{
     protected:
+        int id;
         std::string titulo;
         std::string descricao;
-        std::string status;
         int prioridade;
-        int dia, mes, ano; //data de vencimento
+        std::string dataVencimento; 
     public:
+    //Contrutores
         Tarefa();
-        Tarefa(const std::string& titulo, const std::string descricao, const std::string status, int prioridade, int dia, int mes, int ano);
+        Tarefa(int id, const std::string& titulo, const std::string& descricao, int prioridade, const std::string& dataVencimento) 
+        : id(id), titulo(titulo), descricao(descricao), prioridade(prioridade), dataVencimento(dataVencimento) {}
 
+        Tarefa(int id, const std::string& titulo, const std::string& descricao, int prioridade) 
+        : id(id), titulo(titulo), descricao(descricao), prioridade(prioridade) {}
+
+        Tarefa(int id, const std::string& titulo, const std::string& descricao) 
+        : id(id), titulo(titulo), descricao(descricao) {}
+
+        Tarefa(int id, const std::string& titulo) 
+        : id(id), titulo(titulo) {}
+
+    //Métodos getters
+        int getId() const { return id; }
+        std::string getTitulo() const { return titulo; }
+        std::string getDescricao() const { return descricao; }
+        int  getPrioridade() const { return prioridade; }
+        std::string getVencimento() const { return dataVencimento; }
+
+    //Métodos setters
+        void setId(int id) {this->id = id; }
+        void setTitulo(std::string titulo) { this->titulo = titulo; }
+        void setDescricao(std::string descricao) { this->descricao = descricao; }
+        void setPrioridade(int prioridade) { this->prioridade = prioridade; }
+        void setVencimento(std::string dataVencimento) { this->dataVencimento = dataVencimento; }
+            
 };
 
-class Coluna {
-protected: 
+class KanbanTask : public Tarefa {
+    private:
+        std::string status;
 
-public:
-    void adicionarTarefa();
-    void removerTarefa();
-    void moverTarefa(const Tarefa& tarefa, Coluna& colunaDestino);
-    
+    public:
+    //Contrutores
+        KanbanTask(int id, const std::string& titulo)
+            : Tarefa(id, titulo) {}
+
+        KanbanTask(int id, const std::string& titulo, const std::string& descricao)
+            : Tarefa(id, titulo, descricao) {}
+
+        KanbanTask(int id, const std::string& titulo, const std::string& descricao, int prioridade)
+            : Tarefa(id, titulo, descricao, prioridade) {}
+
+        KanbanTask(int id, const std::string& titulo, const std::string& descricao, int prioridade, const std::string& dataVencimento)
+            : Tarefa(id, titulo, descricao, prioridade, dataVencimento) {}
+
+    //Método set
+        void setStatus(const std::string& status) { this->status = status; }
+        std::string getStatus() const { return status; }
 };
 
-class Backlog : public Coluna {
+class TaskManagement {
 private:
-    Tarefa* listabacklog;
+    ListaDuplamenteEncadeada<Tarefa*> tarefas;
+
 public:
+    void addTask(Tarefa* task) { tarefas.pushBack(task); }
+    void removeTask(Tarefa* task) { tarefas.removeValue(task); }
 
+    //Método get
+    ListaDuplamenteEncadeada<Tarefa*> getTarefas() const { return tarefas; }
+
+    void sortTarefas(){}
+    void moveTask(int taskId, const std::string& sourceStatus, const std::string& destinationStatus) {}
 };
-
-class EmAnalise : public Coluna {
-public:
-
-};
-
-class ParaFazer : public Coluna {
-public:
-
-};
-
-class Fazendo : public Coluna {
-public:
-
-};
-
-class Feito : public Coluna {
-public:
-
-};
-
-
-
-
-
 
 
 #endif
